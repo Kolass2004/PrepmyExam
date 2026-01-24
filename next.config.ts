43d70b9a08e612af601efdb1f29fa592769d2e59
Kolass rexon J <89210243+Kolass2004@users.noreply.github.com>
2026-01-24 12:23:50 +0530

    feat: Configure Next.js with `reactStrictMode`, `images.remotePatterns`, `compiler.removeConsole`, and `experimental.optimizePackageImports`.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "gsap"],
+  },
 };
 
 export default nextConfig;
